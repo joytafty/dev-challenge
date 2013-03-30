b0dfcd14ebe6597f63ed6a93b2398117afb6feea
joytafty <joytafty@gmail.com>
2013-03-30 14:06:07 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,90 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Tharathorn (Joy) Rimchala
+# Problem 2.1
+print 'Problem 2.1'
+print '-------------------'
+print 'Answer is commented in chapter2_exercise.py'
+# zipcode = 02132 command attempts to assign 02132 as an integer to variable ZIPCODE, but int cannot start with 0.
+# zipcode number should be assigned as string (zipcode = "02132") rather than as int. 
+print ''
+
+# Problem 2.2
+print 'Problem 2.2'
+print '-------------------'
+print 5
+# Assign value to x
+x = 5
+print 'x = 5'
+print 'x + 1 = ' + str(x+1)
+print ''
+
+# Problem 2.3
+print 'Problem 2.3'
+print '-------------------'
+# Assign values to variables
+width = 17
+height = 12.0
+delimiter = '.'
+
+a1 = width/2
+print '1. width/2 = ' + str(a1) + ' (' + str(type(a1)) + ')'
+a2 = width/2.0
+print '2. width/2.0 = ' + str(a2) + ' (' + str(type(a2)) + ')'
+a3 = height/3
+print '3. height/3 = ' + str(a3) +  ' (' + str(type(a3)) + ')'
+a4 = 1 + 2 * 5
+print '4. 1 + 2 * 5 = ' + str(a4) + ' (' + str(type(a4)) + ')'
+a5 = delimiter * 5
+print '5. delimiter * 5 = ' + str(a5) + ' (' + str(type(a5)) + ')'
+print ''
+
+# Problem 2.4
+print 'Problem 2.4'
+print '-------------------'
+
+from math import pi 
+# Assign variables 2.4.1
+r = 5.0
+# Compute answers 2.4.1
+v = (4.0/3.0)*pi*r**(3)
+# Output 2.4.1
+print '1. Volume of a sphere with radius 5 is : ' + str(v)
+# Assign variables 2.4.2
+price = 24.95
+discounted = 0.6
+ship1 = 3.0
+shipN = 0.75
+Ncopy = 60.0
+# Compute answer 2.4.2
+bookcost = discounted*price*Ncopy
+shipcost = ship1 + shipN*(Ncopy-1)
+wholecost = bookcost + shipcost
+# Output 2.4.2
+print '2. Total wholesale cost for 60 copies is : ' + str(wholecost)
+
+# Assign variables 2.4.3
+ti_hr = 6
+ti_min = 52
+ti_sec = 0 
+
+# Compute answer 2.4.3
+from math import floor
+dt_easy = (8*60 + 15)*2.0 				#sec
+dt_tempo = (7*60 + 12)*3.0 				#sec
+dt_tot = dt_easy + dt_tempo				#sec
+tis = ti_hr*3600 + ti_min*60 + ti_sec	#sec
+tfs = tis + dt_tot						#sec
+tend_hr = int(floor(tfs/3600))			#hr
+tend_min = int(floor((tfs%3600)/60))	#min
+tend_sec = int(tfs%60)					#sec
+
+# Output 2.4.3
+print ('3(1) Time spent running : ' + 
+	str(int(floor(dt_tot/3600))) + ' hours, ' + 
+	str(int(floor(dt_tot%3600)/60)) + ' mins, ' + 
+	str(int(floor(dt_tot%60))) + ' secs')
+print ('3(2) Time I get home : ' + 
+	str(tend_hr) + ':' + 
+	str(tend_min) + ':' + 
+	str(tend_sec) )
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,97 @@
 # Exercises for chapter 3:
+# Name: Tharathorn (Joy) Rimchala
+# Problem 3.1
+print 'Problem 3.1'
+print '-------------------'
 
-# Name:
+# repeat_lyrics()
+
+# def print_lyrics():
+# 		print "I'm a lumberjack, and I'm okay."
+# 		print "I sleep all night and I work all day."
+
+# def repeat_lyrics():
+# 	print_lyrics()
+# 	print_lyrics()
+
+
+print 'Calling function before definition results in the following error message:'
+print """ NameError: name \'repeat_lyrics\' is not defined """
+print 'This error arises because \'repeat_lyrics\' was called before its define declaration.'
+print ''
+
+# Problem 3.2
+print 'Problem 3.2'
+print '-------------------'
+
+# def repeat_lyrics():
+# 	print_lyrics()
+# 	print_lyrics()
+
+# def print_lyrics():
+# 		print "I'm a lumberjack, and I'm okay."
+# 		print "I sleep all night and I work all day."
+
+# repeat_lyrics()
+
+print """Calling \'print_lyrics\' is within \'repeat_lyrics\' in the main script does not cause any error. 
+At this point in the main script, both functions are already defined. As such, the script compiles and runs without errors."""
+print ''
+
+# Problem 3.3
+print 'Problem 3.3'
+print '-------------------'
+def right_justify(instr):
+	endpos = 70
+	strpos = endpos - len(instr) + 1
+	outpos = ' '*strpos + instr
+
+	print outpos
+
+s = 'RampUp'
+print 'right_justify(' + s + ') returns :'
+right_justify(s)
+
+print ''
+
+# Problem 3.4
+print 'Problem 3.4'
+print '-------------------'
+print '3.4.1'
+def print_spam():
+	print 'spam'
+
+def do_twice(f):
+	f()
+	f()
+
+do_twice(print_spam)
+print ''
+
+print '3.4.2'
+def print_spam_v2(instr):
+	print 'spam'
+
+def do_twice_v2(f, instr):
+	f(instr)
+	f(instr)
+
+instr = 'spam'
+do_twice_v2(print_spam_v2, instr)
+print ''
+
+print '3.4.3-4'
+def print_twice(instr):
+	print instr
+	print instr
+
+do_twice_v2(print_twice, instr)
+print ''
+
+print '3.4.5'
+def do_four(f, instr):
+	do_twice_v2(f, instr)
+	do_twice_v2(f, instr)
+
+do_four(print_twice, instr)
+print ''
